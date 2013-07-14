0e5e3783ca052cfb9d219b40be3004cf59e012b9
Ritwik Desai <ritwikdesai@Ritwiks-MacBook-Pro.local>
2013-07-15 01:38:21 +0530

    a

---
+++ CampusBuddy/SubjectCell.m
@@ -0,0 +1,30 @@
+//
+//  SubjectCell.m
+//  CampusBuddy
+//
+//  Created by Ritwik Desai on 13/07/13.
+//  Copyright (c) 2013 Ritwik Desai. All rights reserved.
+//
+
+#import "SubjectCell.h"
+
+@implementation SubjectCell
+
+@synthesize isNew = _isNew;
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
